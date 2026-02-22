83c86cc42a6ed20c1bb3205202f6d5837ec26ab4
ambuj vashistha <ambujva123@gmail.com>
2026-02-23 00:37:37 +0530

    home page header section

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,9 +1,208 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ScrollView, StyleSheet, TouchableOpacity } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.topSection}>
+          <Image
+            source={require("../../assets/doom.jpg")}
+            style={styles.avatar}
+          />
+
+          <View>
+            <Text style={styles.name}>Dr Doom</Text>
+            <Text style={styles.description}>Crazy description of Dr Doom</Text>
+          </View>
+            <TouchableOpacity>
+              <Ionicons name="notifications" size={24} color="grey" />
+            </TouchableOpacity>
+        </View>
+
+        <View style={styles.totalImpact}>
+          <Text style={styles.totalImpactText}>Your Total Impact</Text>
+          <Text style={styles.totalImpactTitle}>12 Families</Text>
+          <Text style={styles.totalImpactText}>
+            Download your FY 2024-25 tax-exempt certificate now.
+          </Text>
+        </View>
+      </View>
+      <ScrollView
+        contentContainerStyle={styles.scrollContent}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Total Impact</Text>
+          <Text style={styles.sectionValue}>$12,345</Text>
+        </View>
+        <View style={styles.impact}>
+          <View style={styles.section}>
+            <Text style={styles.sectionTitle}>Lives Touched</Text>
+            <Text style={styles.sectionValue}>1,234</Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.sectionTitle}>Campaigns</Text>
+            <Text style={styles.sectionValue}>56</Text>
+          </View>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Total Impact</Text>
+          <Text style={styles.sectionValue}>$12,345</Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Total Impact</Text>
+          <Text style={styles.sectionValue}>$12,345</Text>
+        </View>
+
+        <View style={styles.section}>
+          <View style={styles.section}>
+            <TouchableOpacity>
+              <Text>Notification Preferences</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.section}>
+            <TouchableOpacity>
+              <Text>Security & App PIN</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.section}>
+            <TouchableOpacity>
+              <Text style={styles.signOutText}>Sign Out</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    flex: 1,
+    backgroundColor: "#F5F6F8",
+  },
+  scrollContent: {
+    padding: 20,
+  },
+  header: {
+    alignItems: "center",
+    marginBottom: 30,
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginTop: 12,
+  },
+  description: {
+    fontSize: 14,
+    fontWeight: "400",
+    marginTop: 2,
+  },
+  impact: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+  },
+  section: {
+    backgroundColor: "#fff",
+    padding: 16,
+    borderRadius: 12,
+    marginBottom: 10,
+  },
+  sectionTitle: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: "#888",
+  },
+  sectionValue: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginTop: 8,
+  },
+  editButton: {
+    marginTop: 12,
+    paddingVertical: 8,
+    paddingHorizontal: 20,
+    backgroundColor: "#c4ebdd",
+    borderRadius: 8,
+  },
+  editButtonText: {
+    color: "#007c4f",
+    fontSize: 14,
+    fontWeight: "500",
+  },
+  signOutText: {
+    color: "#ff4d4d",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  downloadButton: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 12,
+    paddingVertical: 8,
+    paddingHorizontal: 20,
+    backgroundColor: "#ffffff",
+    borderRadius: 8,
+  },
+  downloadButtonText: {
+    color: "#007c4f",
+    fontSize: 14,
+    fontWeight: "500",
+  },
+  sectionTax: {
+    backgroundColor: "#007c4f",
+    padding: 16,
+    borderRadius: 12,
+    marginBottom: 10,
+    marginTop: 10,
+    alignItems: "center",
+  },
+  sectionTaxTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#ffffff",
+    marginTop: 8,
+  },
+  sectionTaxText: {
+    fontSize: 16,
+    fontWeight: "400",
+    color: "#ffffff",
+    marginTop: 10,
+    marginBottom: 12,
+  },totalImpact: {
+    backgroundColor: "#007c4f",
+    padding: 16,
+    borderRadius: 12,
+    marginBottom: 10,
+    marginTop: 20,
+    alignItems: "center",
+  },
+  totalImpactTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#ffffff",
+    marginTop: 8,
+  },
+  totalImpactText: {
+    fontSize: 16,
+    color: "#ffffff",
+  },
+  topSection: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 16,
+    }
+});
